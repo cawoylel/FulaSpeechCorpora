f35e949cbeb7935248936e2a9fc0ca48b0700349
yaya-sy <yayasysco@gmail.com>
2024-01-11 00:21:42 +0100

    async without session

--- src/scrapers/rfi-ff/rfi_ff.py
+++ src/scrapers/rfi-ff/rfi_ff.py
@@ -35,62 +35,55 @@ def _get_selenium_driver():
     driver = webdriver.Firefox()
     return driver
 
-async def _download_audio(driver, audio_src, output_directory, filename, session):
-    print(audio_src)
-    async with session.get(url=audio_src, cookies=_get_selenium_cookies(driver), headers=headers) as data:
-        if data.status == 200:
-            output_path = Path(output_directory) / "audio"
-            output_path.mkdir(exist_ok=True, parents=True)
-            content = await data.content.read()
-            with open(output_path / f"{filename}.mp3", 'wb') as out_mp3:
-                out_mp3.write(content)
+async def _download_audio(driver, audio_src, output_directory, filename):
+    data = requests.get(url=audio_src, cookies=_get_selenium_cookies(driver), headers=headers, stream=True)
+    if data.status_code == 200:
+        output_path = Path(output_directory) / "audio"
+        output_path.mkdir(exist_ok=True, parents=True)
+        with open(output_path / f"{filename}.mp3", 'wb') as out_mp3:
+            out_mp3.write(data.content)
 
-async def bounded_fetch(sem, driver, audio_src, output_directory, filename, session):
+async def bounded_fetch(sem, driver, audio_src, output_directory, filename):
     async with sem:
-        await _download_audio(driver, audio_src, output_directory, filename, session)
+        await _download_audio(driver, audio_src, output_directory, filename)
 
 def get_text(response):
     print(response)
 
 async def download_data(urls, output_directory):
-    conn = aiohttp.TCPConnector(
-            family=socket.AF_INET,
-            verify_ssl=False,
-        )
     driver = _get_selenium_driver()
     accepted = False
-    batch_size = 100
+    batch_size = 200
     sem = asyncio.Semaphore(batch_size)  # Generally, most OS's don't allow you to make more than 1024 sockets unless you personally fine-tuned your system. 
     tasks = []
     n = len(urls)
     batches = [urls[ndx:min(ndx + batch_size, n)] for ndx in range(0, n, batch_size)]
-    for batch in tqdm(batches):
-        async with aiohttp.ClientSession(trust_env=True, connector=conn) as session:
-            for full_url in tqdm(batch):
-                filename = str(uuid4())
-                filename = re.sub("-", "_", filename)
-                driver.get(full_url)
-                if not accepted:
-                    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//button[@id="didomi-notice-agree-button"]'))).click()
-                    accepted = True
-                audio_element = driver.find_element(By.XPATH, "//button[@class='m-cta m-cta--rounded m-cta--play-pause a-picto-play-pause--play']")
-                ActionChains(driver).move_to_element(audio_element).click().perform()
-                # Download audio
-                audio_player = driver.find_element(By.XPATH, '/html/body/audio')
-                audio_src = audio_player.get_attribute('src')
-                task = asyncio.ensure_future(bounded_fetch(sem, driver, audio_src, output_directory, filename, session))
-                tasks.append(task)
-                text_element = driver.find_element(By.XPATH, "//p[@class='t-content__chapo']")
-                title_element = driver.find_element(By.XPATH, "//h1[@class='a-page-title']")
-                text_path = output_directory / "text"
-                text_path.mkdir(exist_ok=True, parents=True)
-                title_path = output_directory / "title"
-                title_path.mkdir(exist_ok=True, parents=True)
-                with open(text_path / f"{filename}.txt", "w") as output_text:
-                    output_text.write(text_element.text)
-                with open(title_path / f"{filename}.title", "w") as output_text:
+    for idx, batch in tqdm(list(enumerate(batches))):
+        for full_url in tqdm(batch, desc=f"{idx}/{len(batches)}"):
+            filename = str(uuid4())
+            filename = re.sub("-", "_", filename)
+            driver.get(full_url)
+            if not accepted:
+                WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//button[@id="didomi-notice-agree-button"]'))).click()
+                accepted = True
+            audio_element = driver.find_element(By.XPATH, "//button[@class='m-cta m-cta--rounded m-cta--play-pause a-picto-play-pause--play']")
+            ActionChains(driver).move_to_element(audio_element).click().perform()
+            # Download audio
+            audio_player = driver.find_element(By.XPATH, '/html/body/audio')
+            audio_src = audio_player.get_attribute('src')
+            task = asyncio.ensure_future(bounded_fetch(sem, driver, audio_src, output_directory, filename))
+            tasks.append(task)
+            text_element = driver.find_element(By.XPATH, "//p[@class='t-content__chapo']")
+            title_element = driver.find_element(By.XPATH, "//h1[@class='a-page-title']")
+            text_path = output_directory / "text"
+            text_path.mkdir(exist_ok=True, parents=True)
+            title_path = output_directory / "title"
+            title_path.mkdir(exist_ok=True, parents=True)
+            with open(text_path / f"{filename}.txt", "w") as output_text:
+                output_text.write(text_element.text)
+            with open(title_path / f"{filename}.title", "w") as output_text:
                     output_text.write(title_element.text)
-            await asyncio.gather(*tasks)
+        await asyncio.gather(*tasks)
 
 
 
