0d520a0049ce5e99f380707bfda6161f4eef74a6
yaya-sy <yayasysco@gmail.com>
2024-01-10 23:49:52 +0100

    async

--- src/scrapers/rfi-ff/rfi_ff.py
+++ src/scrapers/rfi-ff/rfi_ff.py
@@ -3,12 +3,14 @@
 import logging
 import re
 from pathlib import Path
+import asyncio
 import sys
 import requests
 from urllib.parse import urlparse
 from uuid import uuid4
 from tqdm import tqdm
-from scrapy import Request
+import aiohttp
+import socket 
 
 # Selenium imports
 from selenium import webdriver
@@ -33,62 +35,74 @@ def _get_selenium_driver():
     driver = webdriver.Firefox()
     return driver
 
-def _download_audio(driver, audio_src, output_directory, filename):
-    parsed_url = urlparse(audio_src)
-    # Get filename
-    print("REUESTING...")
-    data = requests.get(audio_src, cookies=_get_selenium_cookies(driver), headers=headers, stream=True)
-    print("REUESTING FINISHED")
-    if data.status_code == 200:
-        output_path = Path(output_directory) / "audio"
-        output_path.mkdir(exist_ok=True, parents=True)
-        with open(output_path / f"{filename}.mp3", 'wb') as out_mp3:
-            out_mp3.write(data.content)
+async def _download_audio(driver, audio_src, output_directory, filename, session):
+    print(audio_src)
+    async with session.get(url=audio_src, cookies=_get_selenium_cookies(driver), headers=headers) as data:
+        if data.status == 200:
+            output_path = Path(output_directory) / "audio"
+            output_path.mkdir(exist_ok=True, parents=True)
+            content = await data.content.read()
+            with open(output_path / f"{filename}.mp3", 'wb') as out_mp3:
+                out_mp3.write(content)
+
+async def bounded_fetch(sem, driver, audio_src, output_directory, filename, session):
+    async with sem:
+        await _download_audio(driver, audio_src, output_directory, filename, session)
 
 def get_text(response):
     print(response)
 
-def download_data(urls, output_directory, skip_done):
+async def download_data(urls, output_directory):
+    conn = aiohttp.TCPConnector(
+            family=socket.AF_INET,
+            verify_ssl=False,
+        )
     driver = _get_selenium_driver()
     accepted = False
-    for full_url in tqdm(urls):
-        filename = str(uuid4())
-        filename = re.sub("-", "_", filename)
-        driver.get(full_url)
-        if not accepted:
-            WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//button[@id="didomi-notice-agree-button"]'))).click()
-            accepted = True
-        audio_element = driver.find_element(By.XPATH, "//button[@class='m-cta m-cta--rounded m-cta--play-pause a-picto-play-pause--play']")
-        ActionChains(driver).move_to_element(audio_element).click().perform()
-        # Download audio
-        audio_player = driver.find_element(By.XPATH, '/html/body/audio')
-        audio_src = audio_player.get_attribute('src')
-        _download_audio(driver, audio_src, output_directory, filename)
-        text_element = driver.find_element(By.XPATH, "//p[@class='t-content__chapo']")
-        title_element = driver.find_element(By.XPATH, "//h1[@class='a-page-title']")
-        text_path = output_directory / "text"
-        text_path.mkdir(exist_ok=True, parents=True)
-        title_path = output_directory / "title"
-        title_path.mkdir(exist_ok=True, parents=True)
-        with open(text_path / f"{filename}.txt", "w") as output_text:
-            output_text.write(text_element.text)
-        with open(title_path / f"{filename}.title", "w") as output_text:
-            output_text.write(title_element.text)
+    batch_size = 100
+    sem = asyncio.Semaphore(batch_size)  # Generally, most OS's don't allow you to make more than 1024 sockets unless you personally fine-tuned your system. 
+    tasks = []
+    n = len(urls)
+    batches = [urls[ndx:min(ndx + batch_size, n)] for ndx in range(0, n, batch_size)]
+    for batch in tqdm(batches):
+        async with aiohttp.ClientSession(trust_env=True, connector=conn) as session:
+            for full_url in tqdm(batch):
+                filename = str(uuid4())
+                filename = re.sub("-", "_", filename)
+                driver.get(full_url)
+                if not accepted:
+                    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH,'//button[@id="didomi-notice-agree-button"]'))).click()
+                    accepted = True
+                audio_element = driver.find_element(By.XPATH, "//button[@class='m-cta m-cta--rounded m-cta--play-pause a-picto-play-pause--play']")
+                ActionChains(driver).move_to_element(audio_element).click().perform()
+                # Download audio
+                audio_player = driver.find_element(By.XPATH, '/html/body/audio')
+                audio_src = audio_player.get_attribute('src')
+                task = asyncio.ensure_future(bounded_fetch(sem, driver, audio_src, output_directory, filename, session))
+                tasks.append(task)
+                text_element = driver.find_element(By.XPATH, "//p[@class='t-content__chapo']")
+                title_element = driver.find_element(By.XPATH, "//h1[@class='a-page-title']")
+                text_path = output_directory / "text"
+                text_path.mkdir(exist_ok=True, parents=True)
+                title_path = output_directory / "title"
+                title_path.mkdir(exist_ok=True, parents=True)
+                with open(text_path / f"{filename}.txt", "w") as output_text:
+                    output_text.write(text_element.text)
+                with open(title_path / f"{filename}.title", "w") as output_text:
+                    output_text.write(title_element.text)
+            await asyncio.gather(*tasks)
 
-def get_bible_language_data(urls_file, output_directory, skip_done):
 
+
+async def main(kwargs):
     # Generate URLs
-    with open(urls_file, "r") as links:
+    with open(kwargs["urls_file"], "r") as links:
         generated_urls = [line.strip() for line in links]
     logger.info("Generated {} URLs.".format(len(generated_urls)))
 
-    full_output_directory = Path(output_directory) / "ff"
+    full_output_directory = Path(kwargs["output_directory"]) / "ff"
     full_output_directory.mkdir(exist_ok=True, parents=True)
-    download_data(generated_urls, full_output_directory, skip_done)
-
-
-def main(**kwargs):
-    get_bible_language_data(**kwargs)
+    await download_data(generated_urls, full_output_directory)
 
 
 def _parse_args(argv):
@@ -112,4 +126,4 @@ if __name__ == '__main__':
     args = _parse_args(argv)
 
     logging.basicConfig(level=logging.INFO)
-    main(**args)
+    asyncio.run(main(args))
